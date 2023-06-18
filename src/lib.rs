4470f1594a886dd99579d6415bc261f1dac19dba
Warhorst <->
2023-06-18 18:33:31 +0200

    Added the now working line intersection into the main algorithm. Removed the extra module, as it is no longer needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
-mod intersection;
-
 use std::collections::HashSet;
+use std::ops::{Add, Div, Mul, Sub};
+
 use ordered_float::OrderedFloat;
 
+use crate::IntersectionStatus::*;
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-pub struct Vector {
+pub struct Vec2 {
     x: OrderedFloat<f32>,
     y: OrderedFloat<f32>,
 }
 
-impl Vector {
+impl Vec2 {
     pub fn new(x: f32, y: f32) -> Self {
         Self {
             x: OrderedFloat::from(x),
@@ -29,90 +31,122 @@ impl Vector {
         self.y.0
     }
 
-    pub fn cross_product(&self, other: Vector) -> f32 {
-        self.x() * other.y() - self.y() * other.x()
-    }
-
-    pub fn diff(&self, other: Vector) -> Vector {
-        Vector::new(
+    pub fn diff(&self, other: Vec2) -> Vec2 {
+        Vec2::new(
             other.x() - self.x(),
             other.y() - self.y(),
         )
     }
 }
 
-#[derive(Copy, Clone)]
-pub struct Line {
-    a: Vector,
-    b: Vector,
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
 
-impl Line {
-    pub fn new(a: Vector, b: Vector) -> Self {
+impl Sub for Vec2 {
+    type Output = Vec2;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec2 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl Mul for Vec2 {
+    type Output = f32;
+
+    /// Cross product between two vectors
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.x() * rhs.y() - self.y() * rhs.x()
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Vec2;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec2 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl Div<f32> for Vec2 {
+    type Output = Vec2;
+
+    fn div(self, rhs: f32) -> Self::Output {
+        Vec2 {
+            x: self.x / rhs,
+            y: self.y / rhs
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct Segment {
+    a: Vec2,
+    b: Vec2,
+}
+
+impl Segment {
+    pub fn new(a: Vec2, b: Vec2) -> Self {
         Self { a, b }
     }
 
     pub fn from_coords(x0: f32, y0: f32, x1: f32, y1: f32) -> Self {
-        Self::new(Vector::new(x0, y0), Vector::new(x1, y1))
+        Self::new(Vec2::new(x0, y0), Vec2::new(x1, y1))
     }
 
-    fn points(&self) -> [Vector; 2] {
+    fn points(&self) -> [Vec2; 2] {
         [self.a, self.b]
     }
 
-    fn calculate_intersection(&self, other: Line) -> Option<Vector> {
-        let slope_0 = self.calculate_slope();
-        let slope_1 = other.calculate_slope();
+    /// Calculate the intersection between this line segment and another one.
+    /// Based on this answer on stack overflow: https://stackoverflow.com/a/565282
+    fn calculate_intersection(&self, other: Segment) -> IntersectionStatus {
+        let p = self.a;
+        let q = other.a;
+        let r = self.b - self.a;
+        let s = other.b - other.a;
 
-        if slope_0 == slope_1 {
-            return None;
+        let r_cross_s = r * s;
+        let q_minus_p = q - p;
+        let q_minus_p_cross_r = q_minus_p * r;
+
+        if r_cross_s == 0.0 && q_minus_p_cross_r == 0.0 {
+            return Collinear;
         }
 
-        let intercept_0 = self.a.y() - slope_0 * self.a.x();
-        let intercept_1 = other.a.y() - slope_1 * other.a.x();
-
-        let x = (intercept_1 - intercept_0) / (slope_0 - slope_1);
-        let y = slope_0 * x + intercept_0;
-
-        if x < self.a.x().min(self.b.x()) || x > self.a.x().max(self.b.x()) || y < self.a.y().min(self.b.y()) || y > self.a.y().max(self.b.y()) ||
-            x < other.a.x().min(other.b.x()) || x > other.a.x().max(other.b.x()) || y < other.a.y().min(other.b.y()) || y > other.a.y().max(other.b.y()) {
-            return None;
+        if r_cross_s == 0.0 && q_minus_p_cross_r != 0.0 {
+            return NotIntersecting;
         }
 
-        Some(Vector::new(x, y))
-    }
+        let t = q_minus_p * (s / r_cross_s);
+        let u = q_minus_p * (r / r_cross_s);
 
-    fn calculate_intersection_(&self, other: Line) -> Option<Vector> {
-        let p0_x = self.a.x();
-        let p0_y = self.a.y();
-        let p1_x = self.b.x();
-        let p1_y = self.b.y();
-        let p2_x = other.a.x();
-        let p2_y = other.a.y();
-        let p3_x = other.b.x();
-        let p3_y = other.b.y();
-
-        let s1_x = p1_x - p0_x;
-        let s1_y = p1_y - p0_y;
-        let s2_x = p3_x - p2_x;
-        let s2_y = p3_y - p2_y;
-
-        let s = (-s1_y * (p0_x - p2_x) + s1_x * (p0_y - p2_y)) / (-s2_x * s1_y + s1_x * s2_y);
-        let t = (s2_x * (p0_y - p2_y) - s2_y * (p0_x - p2_x)) / (-s2_x * s1_y + s1_x * s2_y);
-
-        if (0.0..=1.0).contains(&s) && (0.0..=1.0).contains(&t) {
-            Some(Vector::new(
-                p0_x + (t * s1_x),
-                p0_y + (t * s1_y)
-            ))
-        } else {
-            None
+        if r_cross_s != 0.0 && (0.0..=1.0).contains(&t) && (0.0..=1.0).contains(&u) {
+            return Intersecting(p + r * t);
         }
-    }
 
-    fn calculate_slope(&self) -> f32 {
-        (self.b.y() - self.a.y()) / (self.b.x() - self.a.x())
+        NotIntersecting
     }
+}
+
+#[derive(PartialEq, Debug)]
+enum IntersectionStatus {
+    Intersecting(Vec2),
+    Collinear,
+    NotIntersecting,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -124,30 +158,28 @@ pub struct Triangle {
 
 pub fn raycast(
     origin: (f32, f32),
-    lines: Vec<Line>,
+    lines: Vec<Segment>,
 ) -> Vec<Triangle> {
-    let origin = Vector::new(origin.0, origin.1);
+    let origin = Vec2::new(origin.0, origin.1);
 
     let points = lines
         .iter()
-        .flat_map(Line::points)
+        .flat_map(Segment::points)
         .collect::<HashSet<_>>();
 
     let mut intersection_points = vec![];
 
     for point in points {
-        println!("Point: {:?}", point);
-        let origin_to_point = Line::new(origin, point);
+        let origin_to_point = Segment::new(origin, point);
         let mut nearest_intersection = point;
         let mut nearest_distance = calculate_distance(origin, point);
 
         for line in &lines {
-            if let Some(intersection) = origin_to_point.calculate_intersection(*line) {
+
+            if let Intersecting(intersection) = origin_to_point.calculate_intersection(*line) {
                 let distance_to_intersection = calculate_distance(origin, intersection);
 
                 if distance_to_intersection < nearest_distance {
-                    println!("Last nearest intersection: {:?}", nearest_intersection);
-                    println!("New nearest intersection: {:?}", intersection);
                     nearest_intersection = intersection;
                     nearest_distance = distance_to_intersection
                 }
@@ -162,8 +194,6 @@ pub fn raycast(
         let angle_1 = calculate_angle(origin, *p2);
         angle_0.total_cmp(&angle_1)
     });
-
-    println!("{:?}", intersection_points);
 
     let mut triangles = intersection_points
         .windows(2)
@@ -184,16 +214,16 @@ pub fn raycast(
 }
 
 fn calculate_distance(
-    p1: Vector,
-    p2: Vector,
+    p1: Vec2,
+    p2: Vec2,
 ) -> f32 {
     ((p2.x() - p1.x()).powi(2) + (p2.y() - p1.y()).powi(2)).sqrt()
 }
 
 // TODO degree or rad? Degree for now
 fn calculate_angle(
-    p1: Vector,
-    p2: Vector,
+    p1: Vec2,
+    p2: Vec2,
 ) -> f32 {
     let angle_rad = (p2.y() - p1.y()).atan2(p2.x() - p1.x());
     angle_rad.to_degrees()
@@ -202,29 +232,30 @@ fn calculate_angle(
 
 #[cfg(test)]
 mod tests {
-    use crate::{Line, Vector};
+    use crate::{Segment, Vec2};
+    use crate::IntersectionStatus::*;
 
     #[test]
     fn calculate_intersection_works() {
-        let line = Line::from_coords(0.0, 0.0, 5.0, 0.0);
+        let line = Segment::from_coords(0.0, 0.0, 5.0, 0.0);
 
         [
             (
-                Line::from_coords(3.0, 3.0, 3.0, -3.0),
-                Some(Vector::new(3.0, 0.0))
+                Segment::from_coords(3.0, 3.0, 3.0, -3.0),
+                Intersecting(Vec2::new(3.0, 0.0))
             ),
             (
-                Line::from_coords(0.0, 3.0, 0.0, -3.0),
-                Some(Vector::new(0.0, 0.0))
+                Segment::from_coords(0.0, 3.0, 0.0, -3.0),
+                Intersecting(Vec2::new(0.0, 0.0))
             ),
             (
-                Line::from_coords(5.0, 3.0, 5.0, -3.0),
-                Some(Vector::new(5.0, 0.0))
+                Segment::from_coords(5.0, 3.0, 5.0, -3.0),
+                Intersecting(Vec2::new(5.0, 0.0))
             ),
             (
-                Line::from_coords(0.0, 0.0, 3.0, 0.0),
-                Some(Vector::new(5.0, 0.0))
+                Segment::from_coords(0.0, 0.0, 3.0, 0.0),
+                Collinear
             )
-        ].into_iter().for_each(|(l, intersection)| assert_eq!(line.calculate_intersection_(l), intersection))
+        ].into_iter().for_each(|(l, intersection)| assert_eq!(line.calculate_intersection(l), intersection))
     }
 }
